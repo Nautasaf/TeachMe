748e61e1926b6e61385330eae597a65da0577de4
Nautasaf <salavat01062004@gmail.com>
2025-07-31 16:50:43 +0500

    Server works

--- server/server.js
+++ server/server.js
@@ -1,4 +1,24 @@
 const express = require('express');
 const serverConfig = require('./serverConfig');
 const server = express();
-const db = require("./models")
+const db = require("./db/models");
+
+serverConfig(server);
+
+async function testConnection() {
+  try {
+    await db.sequelize.authenticate();
+    console.log('БД подключена успешно');
+  } catch (error) {
+    console.log('Ошибка подключения к БД', error);
+  }
+}
+
+testConnection();
+
+// Добавьте определение переменной PORT
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log(`Server is working on port ${PORT}`);
+});
